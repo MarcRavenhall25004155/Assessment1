5b402cea592cdbe3f890b2c2b592c6531472d844
MarcRavenhall25004155 <marcravenhall99@outlook.com>
2025-12-04 12:18:24 +0000

    Created the loop for building the list

---
+++ main.py
@@ -0,0 +1,24 @@
+# Print Menu Message
+
+print("===Create a Register===")
+
+# Create empty list
+
+name_list = []
+
+# Get user to fill list
+def register():
+        while True:
+            name = input("Enter a name: ")
+
+            name_list.append(name)
+
+            again = input("Would you like to add another name? (y/n): ")
+            if again == "n":
+                print(name_list)
+                break
+            elif again == "y":
+                print("Please enter another name")
+
+
+register()
