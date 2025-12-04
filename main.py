ca39f2d187d3ee58f1f52af37aee31420d9d3beb
MarcRavenhall25004155 <marcravenhall99@outlook.com>
2025-12-04 13:12:33 +0000

    Added command to order and print list alphabetically

--- main.py
+++ main.py
@@ -15,10 +15,11 @@ def register():
 
             again = input("Would you like to add another name? (y/n): ")
             if again == "n":
-                print(name_list)
+
+                ordered_list = sorted(name_list)
+                print(ordered_list)
                 break
             elif again == "y":
                 print("Please enter another name")
-
 
 register()
