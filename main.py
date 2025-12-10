1f967ecff77db473b6a580ef3d9d83648ff49b47
MarcRavenhall25004155 <marcravenhall99@outlook.com>
2025-12-10 14:10:41 +0000

    Added timestamps to each entry. Removed list and replaced with Dictionary to store timestamps and name as a tuple. Edited while loop to store timestamps and formatting. Edited For loop to loop through all dictionary items and unpack into variables (name) (time).

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
+import datetime
+
 print("===Create a Register===")
 
-# Create empty list
+# Create Empty Dictionary
+register_dict = {}
 
-name_list = []
-
-# Get user to fill list
+# Get user to fill Dictionary with names whilst also storing the current time they were added
 def register():
         while True:
             name = input("Enter a name: ")
-
-            name_list.append(name)
+            now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") # Formatted time stamp
+            register_dict[name] = now
 
             again = input("Would you like to add another name? (y/n): ").lower()
             if again == "n":
-                ordered_list = sorted(name_list)
                 print(f"\n===Register===")
-                for i in ordered_list:
-                    print(f"\n{i}")
+                # Loop through both key and value at the same time using .items()
+                for name, time in sorted(register_dict.items()): # Unpack Dictionary entries into two variables (name/time)
+                    print(f"{name} - {time}") # time variable has now been assigned the timestamp
                 print(f"\n===End of List===")
                 break
             elif again == "y":
