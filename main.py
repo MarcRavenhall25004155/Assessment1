963346f79348337e0557bf9aeaf6f88bd41316ad
MarcRavenhall25004155 <marcravenhall99@outlook.com>
2025-12-11 12:18:48 +0000

    Code was bulky and hard to read. Created new function to improve readability and usability.

--- main.py
+++ main.py
@@ -5,17 +5,20 @@ print("===Create a Register===")
 # Create Empty Dictionary
 register_dict = {}
 
+def add_entry(name):
+    now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    return name, now
+
 # Get user to fill Dictionary with names whilst also storing the current time they were added
 def register():
         while True:
             name = input("Enter a name: ")
-            now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") # Formatted time stamp
-            register_dict[name] = now
-
+            entry_name, entry_time = add_entry(name)
+            register_dict[entry_name] = entry_time
             again = input("Would you like to add another name? (y/n): ").lower()
             if again == "n":
                 print(f"\n===Register===")
-                # Loop through both key and value at the same time using .items()
+                    # Loop through both key and value at the same time using .items()
                 for name, time in sorted(register_dict.items()): # Unpack Dictionary entries into two variables (name/time)
                     print(f"{name} - {time}") # time variable has now been assigned the timestamp
                 print(f"\n===End of List===")
@@ -23,4 +26,4 @@ def register():
             elif again == "y":
                 print("Please enter another name")
 
-register()
+register() # Call on function to repeat loop
